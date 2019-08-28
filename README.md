1b2b03dd2cebdf4b212cdd5a0d2d85c8634c98c6
caichenhui <864728298@qq.com>
2019-08-28 17:12:56 +0800

    更新一下

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 在白鹭引擎工具流下，使用Babylonjs引擎
 
 项目中完全没有egret相关代码，只是使用egret工具流，即使用egret命令行进行打包发布
+
+babylonjs文档地址[https://doc.babylonjs.com/babylon101/]
--- src/Main.ts
+++ src/Main.ts
@@ -1,31 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2014-present, Egret Technology.
-//  All rights reserved.
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Egret nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
-//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//////////////////////////////////////////////////////////////////////////////////////
 
 class Main  {
 
@@ -36,7 +8,7 @@ class Main  {
     public constructor(canvas:HTMLCanvasElement) {
         this._canvas = canvas;
         this._engine = new BABYLON.Engine(this._canvas, true);
-        this.createBabylonScene();
+        this.createScene();
         this.runRenderLoop();
         window.addEventListener('resize', this.onWindowResize.bind(this));
     }
@@ -44,7 +16,7 @@ class Main  {
     /**
      * 创建babylon场景
      */
-    public createBabylonScene(){
+    private createScene(){
         this._scene = new BABYLON.Scene(this._engine);
 
         var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0,0,5), this._scene);
